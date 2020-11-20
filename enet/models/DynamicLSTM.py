b6054f1753d5785b35aac1d6957a98dab291253f
wren44 <wren44@gatech.edu>
2020-11-21 01:54:08 +0800

    LSTM

--- enet/models/DynamicLSTM.py
+++ enet/models/DynamicLSTM.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 
 class DynamicLSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers=1, bias=True, batch_first=True, dropout=0,
-                 bidirectional=False, device=torch.device("cuda")):
+                 bidirectional=False, device=torch.device("cpu")):
         """
         Dynamic LSTM which can hold variable length sequence, use like TensorFlow's RNN(input, length...).
 
